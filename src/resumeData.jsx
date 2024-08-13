9ecef8f2b48f5aa4459441ea1ce226e7e7af7a65
Sree Dharshni <sreedharshniilango@gmail.com>
2024-08-13 21:54:10 +0530

    first commit

---
+++ src/resumeData.jsx
@@ -0,0 +1,84 @@
+let resumeData = {
+    "imagebaseurl":"https://media.licdn.com/dms/image/v2/D5603AQFx-i3S7585Hg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1706610318882?e=2147483647&v=beta&t=AzWNU65VQHaKkW6opklgPT5vf0QuHJeGb6Ri_M0Gtz0",
+    "name": "Sree Dharshni Ilango",
+    "role": "Frontend Developer and Devops Engineer",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, read books or simply play shuttle in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sreedharshniilango/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Dharshni-19",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"As a recent 2022 graduate, I am eager to apply my skills in AWS, DevOps, HTML, CSS, JavaScript to contribute to innovative projects. With a strong foundation in technology and a passion for learning, I am excited to embark on a rewarding career in the IT industry..",
+    "address":"India",
+    "education":[
+      {
+        "UniversityName":"Anna University",
+        "specialization":"B.Tech, Ceramic Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Foundever",
+        "Role":"Technical support",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2023",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MySql"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Travel and Tourism",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"Bakership",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"E-commerce",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
